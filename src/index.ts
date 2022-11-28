c0a3a793e739fe49e7fe328ddf0383514cd7f474
Lucas Bortoli <lucasbortolisantos@gmail.com>
2022-11-28 08:32:12 -0300

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 import FileSystem from "./fileSystem.js";
+import Webhook from "./webhook.js";
+import DownloadStream from "./streams/download.js";
+import UploadStream from "./streams/upload.js";
+import * as Constants from "./constants.js";
+import * as Utils from "./utils.js";
+import {
+  IFileSystemNode,
+  IFileSystemNodeType,
+  IFileSystemDirectory,
+  IFileSystemFile,
+  FileChunk,
+  FileSystemHeaders,
+} from "./types.js";
 
-export { FileSystem };
+export {
+  FileSystem,
+  Webhook,
+  DownloadStream,
+  UploadStream,
+  Constants,
+  Utils,
+  IFileSystemNode,
+  IFileSystemNodeType,
+  IFileSystemDirectory,
+  IFileSystemFile,
+  FileChunk,
+  FileSystemHeaders,
+};
